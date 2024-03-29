ce42c52d575dc91db4dc06ec7d97cdd832b4b9de
Bikal <bikaladhikari5@gmail.com>
2024-03-30 10:23:29 +1100

    hooks state react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+        };
+      }
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
